cb2e7a442d937ad5bfa387ca5488ba102d377f3d
Sean3216 <92766104+Sean3216@users.noreply.github.com>
2024-01-30 02:27:38 +0900

    Final (Before ending analysis)

--- House Price Prediction/analysis_preprocess.py
+++ House Price Prediction/analysis_preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np 
+from sklearn.preprocessing import OneHotEncoder
 
 from scipy.stats import chi2_contingency
 from scipy.stats import chi2
@@ -54,13 +55,24 @@ def checkallcatvaluecounts(df):
         print(valco)
         print("=====================================")
 
+def groupratings(x, low, high):
+    if x < low:
+        x = 'Low'
+    elif x > high:
+        x = 'High'
+    else:
+        x = 'Normal'
+    return x
+
 
 ######################################################### PCA #########################################################
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+
 class PrincipalComponentAnalysis():
     def __init__(self):
         self.scaler = StandardScaler()
+        self.pca = None
 
     def explain(self, df):
         scaleddata = self.scaler.fit_transform(df)
@@ -96,10 +108,13 @@ class PrincipalComponentAnalysis():
 
         if n_components == None:
             n_components = len(df.columns)
-        self.pca = PCA(n_components = n_components, svd_solver='full')
 
         if mode == 'train':
+            self.pca = PCA(n_components = n_components, svd_solver='full')
             self.pca.fit(scaleddata)
+        if self.pca == None:
+            print("Please train the model first")
+            return None
         rawoutput = self.pca.transform(scaleddata)
         output = pd.DataFrame(rawoutput, columns=["PC_{}".format(i+1) for i in range(n_components)])
         return output
@@ -179,3 +194,243 @@ def outliersearch(df, threshold = 3):
             outlier_index.append(i) 
         
     return outlier_index
+
+###################################### Preprocessing Class ######################################
+class Preprocessing():
+    def __init__(self, df, test):
+        self.tempdata = df.copy() #for fixing format
+
+        self.data = df.copy() #traindata
+        self.test = test.copy() #testdata
+        self.PCA = PrincipalComponentAnalysis()
+
+        #we need categorical data to be in the same format in both train and test data
+        #so, assuming that we know the categories in the categorical features, set the categorical features before getting the dummies
+        self.categories = {'MSSubClass': ['20','30','40','45','50','60','70','75','80','85','90','120','150','160','180','190'],
+                           'MSZoning': ['A','C','FV','I','RH','RL','RP','RM','C (all)'],
+                           'LotShape': ['Reg','IR1','IR2','IR3'],
+                           'LandContour': ['Lvl','Bnk','HLS','Low'],
+                           'LotConfig': ['Inside','Corner','CulDSac','FR2','FR3'],
+                           'Neighborhood': ['Blmngtn','Blueste','BrDale','BrkSide','ClearCr','CollgCr','Crawfor','Edwards','Gilbert','IDOTRR','MeadowV','Mitchel','Names','NoRidge','NPkVill','NridgHt','NWAmes','OldTown','SWISU','Sawyer','SawyerW','Somerst','StoneBr','Timber','Veenker'],
+                           'Condition1': ['Artery','Feedr','Norm','RRNn','RRAn','PosN','PosA','RRNe','RRAe'],
+                           'BldgType': ['1Fam','2fmCon','Duplex','TwnhsE','Twnhs'],
+                           'HouseStyle': ['1Story','1.5f','2Story','2.5f','SFoyer','SLvl'],
+                           'RoofStyle': ['Flat','Gable','Gambrel','Hip','Mansard','Shed'],
+                           'Exterior1st': ['AsbShng','AsphShn','BrkComm','BrkFace','CBlock','CemntBd','HdBoard','ImStucc','MetalSd','Other','Plywood','PreCast','Stone','Stucco','VinylSd','Wd Sdng','WdShing'],
+                           'Exterior2nd': ['AsbShng','AsphShn','BrkComm','BrkFace','CBlock','CemntBd','HdBoard','ImStucc','MetalSd','Other','Plywood','PreCast','Stone','Stucco','VinylSd','Wd Sdng','WdShing'],
+                           'MasVnrType': ['BrkCmn','BrkFace','CBlock','None','Stone'],
+                           'ExterQual': ['Ex','Gd','TA','Fa','Po'],
+                           'ExterCond': ['Ex','Gd','TA','Fa','Po'],
+                           'Foundation': ['BrkTil','CBlock','PConc','Slab','Stone','Wood'],
+                           'BsmtQual': ['Ex','Gd','TA','Fa','Po','NA'],
+                           'BsmtExposure': ['Gd','Av','Mn','No','NA'],
+                           'BsmtFinType1': ['GLQ','ALQ','BLQ','Rec','LwQ','Unf','NA'],
+                           'BsmtFinType2': ['GLQ','ALQ','BLQ','Rec','LwQ','Unf','NA'],
+                           'HeatingQC': ['Ex','Gd','TA','Fa','Po'],
+                           'KitchenQual': ['Ex','Gd','TA','Fa','Po'],
+                           'FireplaceQu': ['Ex','Gd','TA','Fa','Po','NA'],
+                           'GarageType': ['2Types','Attchd','Basment','BuiltIn','CarPort','Detchd','NA'],
+                           'GarageFinish': ['Fin','RFn','Unf','NA'],
+                           'SaleType': ['WD','CWD','VWD','New','COD','Con','ConLw','ConLI','ConLD','Oth'],
+                           'SaleCondition': ['Normal','Abnorml','AdjLand','Alloca','Family','Partial'],
+                           'has2ndfloor': ['Yes','No'],
+                           'hasfireplace': ['Yes','No'],
+                           'multiplecarspace': ['Yes','No'],
+                           'OverallQual': ['Low','Normal','High'],
+                           'OverallCond': ['Low','Normal','High']}
+                           
+
+    def wrangletrain(self):
+        self.data.drop(['Id'],axis = 1, inplace = True)
+
+        ########### Numerical Var Preprocessing ###########
+        #LotFrontage
+        LFneighborhood = self.data.groupby('Neighborhood')['LotFrontage'].median().to_dict()
+        self.data['LotFrontage'] = self.data.apply(lambda row: LFneighborhood[row['Neighborhood']] if np.isnan(row['LotFrontage']) else row['LotFrontage'], axis=1)
+        self.data['LotFrontage'] = self.data['LotFrontage']**2
+        #MasVnrArea
+        self.data['MasVnrArea'].fillna(0, inplace=True)
+
+        #GarageYrBlt
+        self.data['GarageYrBlt'].fillna(0, inplace=True)
+
+        #Convert MSSubClass to categorical
+        self.data['MSSubClass'] = self.data['MSSubClass'].astype('object')
+
+        #### Feature Engineering ####
+        ##This follows the reference provided by Ineeji
+        
+        #add more weight if the building is remodeled recently
+        self.data['YrBltAndRemod']=self.data['YearBuilt']+self.data['YearRemodAdd']
+        
+        #captures the feeling of spaciness in the house
+        self.data['TotalSF']=self.data['TotalBsmtSF'] + self.data['1stFlrSF'] + self.data['2ndFlrSF']
+        self.data['Total_sqr_footage'] = (self.data['BsmtFinSF1'] + self.data['BsmtFinSF2'] +
+                                        self.data['1stFlrSF'] + self.data['2ndFlrSF'])
+        self.data['Total_porch_sf'] = (self.data['OpenPorchSF'] + self.data['3SsnPorch'] +
+                                    self.data['EnclosedPorch'] + self.data['ScreenPorch'] +
+                                    self.data['WoodDeckSF'])
+        self.data['Total_Bathrooms'] = (self.data['FullBath'] + (0.5 * self.data['HalfBath']) +
+                                    self.data['BsmtFullBath'] + (0.5 * self.data['BsmtHalfBath']))
+        
+        #Grouping ratings
+        self.data['OverallQual'] = self.data['OverallQual'].apply(lambda x: groupratings(x, 5, 8)).astype('object')
+        self.data['OverallCond'] = self.data['OverallCond'].apply(lambda x: groupratings(x, 5, 7)).astype('object')
+
+        #Drop GarageArea. It's represented by GarageCars
+        self.data.drop(columns = ['GarageArea'], inplace = True)
+        
+        ########### Categorical Var Preprocessing ###########
+        columnstobedropped = ['PoolQC','MiscFeature','Alley','Fence','Street','Utilities',
+                              'LandSlope','Condition2','RoofMatl','BsmtCond','Heating',
+                              'CentralAir','Electrical','Functional','GarageQual',
+                              'GarageCond','PavedDrive']
+        self.data.drop(columnstobedropped,axis = 1, inplace = True)
+
+        #filling missing values
+        self.data['HouseStyle'] = self.data['HouseStyle'].apply(lambda x: '1.5f' if x == '1.5Fin' or x == '1.5Unf' else x)
+        self.data['HouseStyle'] = self.data['HouseStyle'].apply(lambda x: '2.5f' if x == '2.5Fin' or x == '2.5Unf' else x)
+
+        self.data.loc[(self.data['MasVnrType'].isna()) & (self.data['MasVnrArea'] > 0), 'MasVnrType'] = self.data['MasVnrType'].mode()[0]
+        self.data['MasVnrType'].fillna('None',inplace = True)
+
+        self.data[['BsmtQual','BsmtExposure','BsmtFinType1','BsmtFinType2',
+                   'FireplaceQu','GarageType','GarageFinish']].fillna('NA',inplace = True)
+        #dropping index 948
+        print("Dropping index 948 because of this weird row")
+        print("Value when BsmtExposure is NA but BsmtFinSF1 and BsmtUnfSF is not NA")
+        print(self.data[self.data['BsmtExposure'].isna()][['BsmtFinSF1','BsmtUnfSF']].loc[948])
+        self.data = self.data.drop(index = 948).reset_index(drop = True)
+
+        #Feature Engineering
+        self.data['has2ndfloor'] = self.data['2ndFlrSF'].apply(lambda i: "Yes" if i > 0 else "No").astype('object')
+        self.data['hasfireplace'] = self.data['Fireplaces'].apply(lambda i: "Yes" if i > 0 else "No").astype('object')
+        self.data['multiplecarspace'] = self.data['GarageCars'].apply(lambda i: "Yes" if i > 1 else "No").astype('object')
+        
+        ########### Finishing Preprocessing ###########
+        target = self.data['SalePrice']
+
+        numeric = self.PCA.do(self.data.select_dtypes(exclude = ['object']).drop(['SalePrice'], axis = 1),
+                              n_components= 18, mode = 'train')
+
+        categorical = self.data.select_dtypes(include = ['object'])
+        categorical = self.objecttocategories(categorical)
+
+        self.data = pd.concat([numeric,categorical,target],axis = 1)
+
+        self.data = pd.get_dummies(self.data)
+        return self.data
+    
+    def wrangletest(self):
+        self.test.drop(['Id'],axis = 1, inplace = True)
+
+        ########### Numerical Var Preprocessing ###########
+        #LotFrontage
+        LFneighborhood = self.test.groupby('Neighborhood')['LotFrontage'].median().to_dict()
+        self.test['LotFrontage'] = self.test.apply(lambda row: LFneighborhood[row['Neighborhood']] if np.isnan(row['LotFrontage']) else row['LotFrontage'], axis=1)
+        self.test['LotFrontage'] = self.test['LotFrontage']**2
+
+        #MasVnrArea
+        self.test['MasVnrArea'].fillna(0, inplace=True)
+
+        #GarageYrBlt
+        self.test['GarageYrBlt'].fillna(0, inplace=True)
+
+        #Convert MSSubClass to categorical
+        self.test['MSSubClass'] = self.test['MSSubClass'].astype('object')
+
+        #Before Feature Engineering, there might be missing values in other features that does not exist in the training set
+        #accomodate them with KNNImputer
+        from sklearn.impute import KNNImputer
+        imputer = KNNImputer(n_neighbors=5)
+        tempnumeric = self.test.select_dtypes(exclude = ['object'])
+        tempnumericcol = tempnumeric.columns
+        imputer.fit(self.test.select_dtypes(exclude = ['object']))
+        numericimputed = pd.DataFrame(imputer.transform(tempnumeric),columns = tempnumericcol)
+        self.test.drop(columns = tempnumericcol, inplace = True)
+        self.test = pd.concat([self.test,numericimputed],axis = 1)
+
+        #### Feature Engineering ####
+        ##This follows the reference provided by Ineeji
+        
+        #add more weight if the building is remodeled recently
+        self.test['YrBltAndRemod']=self.test['YearBuilt']+self.test['YearRemodAdd']
+        
+        #captures the feeling of spaciness in the house
+        self.test['TotalSF']=self.test['TotalBsmtSF'] + self.test['1stFlrSF'] + self.test['2ndFlrSF']
+        self.test['Total_sqr_footage'] = (self.test['BsmtFinSF1'] + self.test['BsmtFinSF2'] +
+                                        self.test['1stFlrSF'] + self.test['2ndFlrSF'])
+        self.test['Total_porch_sf'] = (self.test['OpenPorchSF'] + self.test['3SsnPorch'] +
+                                    self.test['EnclosedPorch'] + self.test['ScreenPorch'] +
+                                    self.test['WoodDeckSF'])
+        self.test['Total_Bathrooms'] = (self.test['FullBath'] + (0.5 * self.test['HalfBath']) +
+                                    self.test['BsmtFullBath'] + (0.5 * self.test['BsmtHalfBath']))
+        
+        #Grouping ratings
+        self.test['OverallQual'] = self.test['OverallQual'].apply(lambda x: groupratings(x, 5, 8)).astype('object')
+        self.test['OverallCond'] = self.test['OverallCond'].apply(lambda x: groupratings(x, 5, 7)).astype('object')
+
+        #Drop GarageArea. It's represented by GarageCars
+        self.test.drop(columns = ['GarageArea'], inplace = True)
+        
+        ########### Categorical Var Preprocessing ###########
+        columnstobedropped = ['PoolQC','MiscFeature','Alley','Fence','Street','Utilities',
+                              'LandSlope','Condition2','RoofMatl','BsmtCond','Heating',
+                              'CentralAir','Electrical','Functional','GarageQual',
+                              'GarageCond','PavedDrive']
+        self.test.drop(columnstobedropped,axis = 1, inplace = True)
+
+        self.test['HouseStyle'] = self.test['HouseStyle'].apply(lambda x: '1.5f' if x == '1.5Fin' or x == '1.5Unf' else x)
+        self.test['HouseStyle'] = self.test['HouseStyle'].apply(lambda x: '2.5f' if x == '2.5Fin' or x == '2.5Unf' else x)
+        #filling test data categorical features' missing values is also not known
+        self.test.loc[(self.test['MasVnrType'].isna()) & (self.test['MasVnrArea'] > 0), 'MasVnrType'] = self.test['MasVnrType'].mode()[0]
+        self.test['MasVnrType'].fillna('None',inplace = True)
+
+        self.test[['BsmtQual','BsmtExposure','BsmtFinType1','BsmtFinType2',
+                   'FireplaceQu','GarageType','GarageFinish']].fillna('NA',inplace = True)
+        #fill other missing values with mode
+        self.test.select_dtypes(include = ['object']).fillna(self.test.select_dtypes(include = ['object']).mode().iloc[0], inplace = True)
+
+        #Feature Engineering
+        self.test['has2ndfloor'] = self.test['2ndFlrSF'].apply(lambda i: "Yes" if i > 0 else "No").astype('object')
+        self.test['hasfireplace'] = self.test['Fireplaces'].apply(lambda i: "Yes" if i > 0 else "No").astype('object')
+        self.test['multiplecarspace'] = self.test['GarageCars'].apply(lambda i: "Yes" if i > 1 else "No").astype('object')
+
+        ########### Finishing Preprocessing ###########
+        #Fixing format
+        self.fixingformat()
+
+        numeric = self.PCA.do(self.test.select_dtypes(exclude = ['object']),
+                              n_components= 18, mode = 'test')
+
+        categorical = self.test.select_dtypes(include = ['object'])
+        categorical = self.objecttocategories(categorical)
+        
+        self.test = pd.concat([numeric,categorical],axis = 1)
+
+        self.test = pd.get_dummies(self.test)
+        return self.test
+    
+    def objecttocategories(self, df):
+        categoricalcolumns = self.categories.keys()
+        df = df[categoricalcolumns]
+        cat_as_list = [self.categories[col] for col in df.columns]
+        OHC = OneHotEncoder(categories=cat_as_list, handle_unknown='ignore')
+        OHC.fit(df)
+        df = pd.DataFrame(OHC.transform(df).toarray(), columns=OHC.get_feature_names_out())
+        return df
+    
+    def fixingformat(self):
+        #we also need to make sure that the test data has the same datatype as the train data
+        #so, we need to convert the datatype of the test data to the same as the train data
+        exception = ['SalePrice','Id', 'TotalSF', 'Total_sqr_footage', 'Total_porch_sf', 'Total_Bathrooms', 'HasFireplace','Has2ndfloor','YrBltAndRemod',
+                     'PoolQC','MiscFeature','Alley','Fence','Street','Utilities','LandSlope','Condition2','RoofMatl','BsmtCond','Heating','CentralAir',
+                     'Electrical','Functional','GarageQual','GarageCond','PavedDrive','OverallQual','OverallCond', 'MSSubClass','GarageArea']
+        for col in self.tempdata.columns:
+            if col not in exception:            
+                if self.tempdata[col].dtype != self.test[col].dtype:
+                    print('fixing column {} in test data to fit format {}'.format(col,self.tempdata[col].dtype))
+                    self.test[col] = self.test[col].astype(self.tempdata[col].dtype)
+            else:
+                pass
+    
